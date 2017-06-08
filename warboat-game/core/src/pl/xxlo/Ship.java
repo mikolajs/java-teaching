54bcbf0c08c6d5130259afad0be21bf6eee04566
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2017-06-08 16:27:13 +0200

    warboat change angle dir

--- warboat-game/core/src/pl/xxlo/Ship.java
+++ warboat-game/core/src/pl/xxlo/Ship.java
@@ -7,9 +7,12 @@ import com.badlogic.gdx.math.MathUtils;
 public class Ship {
     final float maxDir = 120f;
     final float startDirLauncher = -90f;
+    final float rotationHull = 90f;
     final float startDirGun = 90f;
     final float maxPower = 20f;
     final float minPower = -10f;
+
+
     final float maxRudder = 20f;
     float speed = 0f;
     float power = 0f;
@@ -52,11 +55,11 @@ public class Ship {
     }
 
     public void render(SpriteBatch batch) {
-        hull.setRotation(90f);
+        hull.setRotation(rotationHull);
         hull.draw(batch);
-        gun.setRotation(gunDir);
+        gun.setRotation(angleToScreen(this.gunDir, this.startDirGun));
         gun.draw(batch);
-        launcher.setRotation(launcherDir);
+        launcher.setRotation(angleToScreen(launcherDir, startDirLauncher);
         launcher.draw(batch);
         redDot.draw(batch);
         takeInput(0L);
@@ -83,19 +86,21 @@ public class Ship {
                 redDot.setCenter(launcher.getX() + launcher.getWidth() / 2f,
                         launcher.getY() + launcher.getHeight() / 2f );
             } else {
-                System.out.println("someware other point touched!");
+                //System.out.println("someware other point touched!");
                 float angle = countAngleOfWeapon(x, y);
+                System.out.println(String.format("kliked %d %d angle %f", x, y, angle ) );
+                float delta = 0;
                 if(mainGun) {
-                    if (Math.abs(angle - gunDir) < 1f) {
-                        //fire weapon
-                    } else {
-                        //rotate weapon
+                    delta = angle - gunDir;
+                    if (Math.abs(delta) > 1f) {
+                        if(delta > 0) rotateGun(false);
+                        else rotateGun(true);
                     }
-                } else {
-                    if(Math.abs(angle - launcherDir) < 1f){
-
-                    } else {
-
+                }  else {
+                    delta = angle - gunDir;
+                    if(Math.abs(delta) > 1f){
+                        if(delta > 0) rotateLuncher(false);
+                        else rotateLuncher(true);
                     }
                 }
             }
@@ -107,43 +112,54 @@ public class Ship {
     }
 
     private boolean isTouched(Sprite s, int x, int y){
-        System.out.println(String.format("touch (%d, %d) sprite position (%f, %f)" +
-                " size (%f, %f)",
-                x, y, s.getX(), s.getY(), s.getWidth(), s.getHeight()));
+        //System.out.println(String.format("touch (%d, %d) sprite position (%f, %f)" +
+        //        " size (%f, %f)",
+        //        x, y, s.getX(), s.getY(), s.getWidth(), s.getHeight()));
         return (x > s.getX()  && x < (s.getX() + s.getWidth())
             && y > s.getY() && y < (s.getY() + s.getHeight()));
     }
 
+    /* @return in degrees by up screen to clock wise
+     */
     private float countAngleOfWeapon(int x, int y){
         float X = centerX;
         float Y = centerY;
         if(this.mainGun)  X -= 19f;
         else X += 20f;
-        float dx = Math.abs(X - (float) x);
-        float dy = Math.abs(Y - (float) y);
-        float alpha = MathUtils.atan2(dy, dx);
-        return alpha * MathUtils.radiansToDegrees;
+        float dx =  (float) x - X;
+        float dy =  Y - (float) y;
+        System.out.println(String.format("tangens: %f for dx %f dy %f",
+                Math.atan2(dx, dy), dx, dy));
+        double alpha = (Math.atan2((double)dy, (double)dx)* (180.0/Math.PI));
+        if(alpha < 0 ) return (float)(360.0 + alpha);
+        else return  (float) alpha;
     }
 
     private void rotateGun(boolean left){
-        if(left) {
-            if(this.gunDir > (this.startDirGun - this.maxDir))
+    if(left) {
+        if(this.gunDir < this.maxDir)
+            this.gunDir += 3f;
+    } else {
+        if(this.gunDir > this.maxDir)
                 this.gunDir -= 3f;
-        } else {
-            if(this.gunDir < (this.startDirGun + this.maxDir))
-                this.gunDir += 3f;
         }
     }
+
 
     private  void rotateLuncher(boolean left){
         if(left) {
-            if(this.launcherDir > (this.startDirLauncher - this.maxDir))
-                this.launcherDir -= 3f;
-        } else {
-            if(this.launcherDir < (this.startDirLauncher + this.maxDir))
+            if(this.launcherDir < this.maxDir  )
                 this.launcherDir += 3f;
+        } else {
+            if(this.launcherDir >  this.maxDir)) {
+                this.launcherDir -= 3f;
+            }
         }
-
     }
 
+    private float angleToScreen(float alpha, float dAlpha) {
+        float angle = alpha + dAlpha;
+        if(angle > 180.0f) angle -= 360.0f;
+        return angle;
+    }
 }
