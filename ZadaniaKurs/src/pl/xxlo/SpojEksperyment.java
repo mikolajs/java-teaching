c61a2e4dc9389703f171bc1687095c7f6f7206ea
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2017-11-08 16:26:37 +0100

    course work, experiment spoj

---
+++ ZadaniaKurs/src/pl/xxlo/SpojEksperyment.java
@@ -0,0 +1,33 @@
+package pl.xxlo;
+
+import java.util.Scanner;
+
+class P {
+    int x;
+    int t;
+}
+
+public class SpojEksperyment {
+
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in); 
+      int d = sc.nextInt();
+      int n = sc.nextInt();
+      int tl = 2000000000;
+      int tr = 2000000000;
+      //P[] drops = new P[n];
+      P[] bottoms = new P[n+1];
+      int x, t;
+      int j;
+      for(int i = 0; i < n; i++) {
+          x = sc.nextInt();
+          t = sc.nextInt();
+          j = 0;
+          while(j <= i) {
+              
+          }
+      }
+      sc.close();
+    }
+
+}
