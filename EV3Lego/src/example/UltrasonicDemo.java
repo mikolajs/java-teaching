4e5b62f262711492def16fcc26c3b96b043d7c30
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2017-11-13 17:14:22 +0100

    lejos

--- EV3Lego/src/example/UltrasonicDemo.java
+++ EV3Lego/src/example/UltrasonicDemo.java
@@ -18,17 +18,17 @@ public class UltrasonicDemo {
         EV3 ev3 = (EV3) BrickFinder.getLocal();
         TextLCD lcd = ev3.getTextLCD();
         Port port = LocalEV3.get().getPort("S1");
+        UltraSonicSensor sonar = new UltraSonicSensor(port);
         lcd.drawString("Wcisnij przycisk ", 1, 1);
         Button.waitForAnyPress();
-        UltraSonicSensor sonar = new UltraSonicSensor(port);
         Button.LEDPattern(4);
         float range;
         for(;;) {
             range = sonar.getRange();
             lcd.drawString(String.format("dist= %f",range), 1, 3);
             if(range < 1.0f) Sound.beepSequenceUp();
-            Delay.msDelay(500); 
-            if(range < 10) break;
+            Delay.msDelay(1000); 
+            if(range < 0.1f) break;
         }
         sonar.close();
     }
