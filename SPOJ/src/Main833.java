3baabbda860d41c68bae5d509612a9e9281ac2a5
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2017-12-29 15:38:25 +0100

    spoj dwumian

--- SPOJ/src/Main833.java
+++ SPOJ/src/Main833.java
@@ -1,36 +1,34 @@
 import java.util.Scanner;
 
-/* BINOMS - Dwumiany */
+/* BINOMS - Dwumiany 
+ * np bionom 32 16 = 601080390. 
+ * mnożenie do 17 do 32 przekroczy zakres long 
+ * binom 30 15 = 155117520
+ * binom 300 296 = 330791175
+ * binom 450 4 = 1685905200
+ * */
 public class Main833 {
-    public static void main(String[] args) {
-        System.out.println("");
-        Scanner sc = new Scanner(System.in);
-        int D = sc.nextInt();
-        int n = 0;
-        int k = 0;
-        for(int i = 0; i < D; i++) {
-            n = sc.nextInt();
-            k = sc.nextInt();
-            System.out.println( silnia(n)/(silnia(k)*silnia(n-k)));
-        }
-        int top = 1;
-        int bottom = 1;
-        if(k > n - k) {
-            for(int i = k; i < n; i++) top *= k;        
-            System.out.println(top/silnia(n-k));
-            
-        } else {
-            //for(int i = )
-        }
-        sc.close();
-    }
-    
-    public static int silnia(int n) {
-        int r = 1;
-        while(n > 1) {
-            r *= n;
-            n--;
-        }
-        return r;
-    }
+	public static void main(String[] args) {
+		System.out.println("");
+		Scanner sc = new Scanner(System.in);
+		int D = sc.nextInt();
+		int n = 0;
+		int k = 0;
+		long b = 0;
+		
+		for (int j = 0; j < D; j++) {
+			n = sc.nextInt();
+			k = sc.nextInt();
+			if(k > n -k) k = n - k;
+			b = 1L;
+			for(int i = 1; i <= k; i++) {
+				b *= (n - i + 1);
+				b /= i;
+			}
+			System.out.println(b);
+		}
+		sc.close();
+	}
+
+	
 }
