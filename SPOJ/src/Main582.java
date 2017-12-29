078b136b583b9f026e43e382c1eec90b5553c950
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2017-12-29 14:14:36 +0100

    spoj binoms

---
+++ SPOJ/src/Main582.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+/* MN_2 */
+public class Main582 {
+    public static void main(String[] args) {
+        System.out.println("");
+        Scanner sc = new Scanner(System.in);
+       
+        sc.close();
+    }
+}
--- SPOJ/src/Main833.java
+++ SPOJ/src/Main833.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
 
+/* BINOMS - Dwumiany */
 public class Main833 {
     public static void main(String[] args) {
         System.out.println("");
         Scanner sc = new Scanner(System.in);
         int D = sc.nextInt();
-        int n, k;
+        int n = 0;
+        int k = 0;
         for(int i = 0; i < D; i++) {
             n = sc.nextInt();
             k = sc.nextInt();
             System.out.println( silnia(n)/(silnia(k)*silnia(n-k)));
+        }
+        int top = 1;
+        int bottom = 1;
+        if(k > n - k) {
+            for(int i = k; i < n; i++) top *= k;        
+            System.out.println(top/silnia(n-k));
+            
+        } else {
+            //for(int i = )
         }
         sc.close();
     }
