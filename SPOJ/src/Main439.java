e6f3e8d5ec49edec3c65c01427990b017818864b
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2017-12-31 10:57:18 +0100

    spoj TMUL end

--- SPOJ/src/Main439.java
+++ SPOJ/src/Main439.java
@@ -1,4 +1,4 @@
-import java.math.BigInteger;
+//import java.math.BigInteger;
 import java.util.Scanner;
 
 /* TMUL - Not So Fast Multiplication */
@@ -11,42 +11,49 @@ public class Main439 {
         for(int i = 0; i < n; i++) {
         	a = sc.next().trim();
         	b = sc.next().trim();
-        	System.out.println("bigInt = " + testBigInt(a, b));
-        	System.out.println("solution = " + solution(a, b));
-        	assert testBigInt(a, b) == solution(a, b);
+//        	System.out.println("bigInt = " + testBigInt(a, b));
+//        	System.out.println("solution = " + solution(a, b));
+//        	assert testBigInt(a, b).equals(solution(a, b));
+        	System.out.println(solution(a, b));
         }
         sc.close();
     }
     
     
-    public static String testBigInt(String a, String b) {
-    	BigInteger bint1 = new BigInteger(a);
-        return bint1.multiply(new BigInteger(b)).toString();
-    }
+//    public static String testBigInt(String a, String b) {
+//    	BigInteger bint1 = new BigInteger(a);
+//        return bint1.multiply(new BigInteger(b)).toString();
+//    }
     
     public static String solution(String a, String b) {
     	char[] A = a.toCharArray();
     	char[] B = b.toCharArray();
     	//System.out.println(A);
-    	int[] C = new int[A.length+B.length-1];
+    	int[] C = new int[A.length+B.length];
     	//mnożenie
     	for(int i = B.length - 1; i >= 0; i--)
     		for(int j = A.length - 1; j >= 0; j--) {
-    		  System.out.println(((int) B[i] - 48) + " * " + ((int) A[j] - 48) + " = " + (((int) B[i]) -48) * (((int) A[j] - 48)));
-    		   C[i+j] += ((int) B[i] -48) * ((int) A[j] - 48);	
+    		   C[i+j+1] += ((int) B[i] -48) * ((int) A[j] - 48);	
     		}
-    	
-    	for(int i = 0; i < C.length; i++) {
-    		System.out.print(C[i]);
-    	}
+
     	System.out.println();
     	//redukcja
-    	
+    	int num = 0;
+    	for(int i = C.length - 1; i >= 0; i--) {
+    		num = C[i];
+    		C[i] = num % 10;
+    		num /= 10;
+    		if(num > 0) C[i - 1] += num;
+    	}
     	
     	//wydruk
     	StringBuilder sb = new StringBuilder();
-    	for(int d : C) sb.append(d);
-    	
+    	int i = 0;
+    	while(C[i] == 0) i++;
+    	while(i < C.length) {
+    		sb.append(C[i]);
+    		i++;
+    	}    	
     	
     	return sb.toString();
     }
