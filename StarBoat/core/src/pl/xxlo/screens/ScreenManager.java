c902710d1121b66bb3452b2dab760794596e5ccd
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2018-02-15 20:00:52 +0100

    restore screens in StarBoat

---
+++ StarBoat/core/src/pl/xxlo/screens/ScreenManager.java
@@ -0,0 +1,32 @@
+package pl.xxlo.screens;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import pl.xxlo.*;
+
+public class ScreenManager {
+
+	final private WarBoatGame game;
+	public ScreenManager(WarBoatGame game) {
+		this.game = game;
+		setStartScreen();
+	}
+	public void setStartScreen() {
+		SplashScreen splash = new SplashScreen(this);
+		game.setScreen(splash);
+	}
+	public void setHelpScreen() {
+		HelpScreen help = new HelpScreen(this);
+		game.setScreen(help);
+	}
+	public void setGameScreen() {
+		GameScreen gameScr = new GameScreen(this);
+		game.setScreen(gameScr);
+	}
+	public void setGameOverScreen() {
+		
+	}
+	
+	public SpriteBatch getBatch() {
+		return game.getBatch();
+	}
+}
