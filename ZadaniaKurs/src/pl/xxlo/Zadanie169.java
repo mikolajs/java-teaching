eadf317e317d0cfca11462ed8d5a6daf24f02a5e
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2018-02-15 12:15:50 +0100

    add exercises

---
+++ ZadaniaKurs/src/pl/xxlo/Zadanie169.java
@@ -0,0 +1,14 @@
+package pl.xxlo;
+
+import java.util.*;
+
+/* matura R2012 */
+public class Zadanie169 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Zobacz matura rozszerzona 2012 zadanie 4");
+
+	}
+
+}
