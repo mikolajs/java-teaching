49f99d68cd097da03fea637eaed2970643c76115
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2018-04-03 23:16:53 +0200

    shop assistant list view

--- ShopingAssistant/app/src/main/java/osp/edu/pl/shopingassistant/ScrollingActivity.java
+++ ShopingAssistant/app/src/main/java/osp/edu/pl/shopingassistant/ScrollingActivity.java
@@ -8,22 +8,58 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    ListView itemsList;
+    String message = null;
+    GoodsArrayAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.shoping_list);
-        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setContentView(R.layout.scroll_activity);
+        Toolbar toolbar =  findViewById(R.id.main_toolbar);
+        toolbar.setNavigationIcon(R.mipmap.ic_trolley);
         setSupportActionBar(toolbar);
+        itemsList = findViewById(R.id.listView);
+        customAdapter = new GoodsArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_multiple_choice,
+                new ArrayList<String>());
+        itemsList.setAdapter(customAdapter);
+
+//        itemsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, final View view,
+//                                    int position, long id) {
+//                final String item = (String) parent.getItemAtPosition(position);
+//                view.animate().setDuration(2000).alpha(0)
+//                        .withEndAction(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                list.remove(item);
+//                                adapter.notifyDataSetChanged();
+//                                view.setAlpha(1);
+//                            }
+//                        });
+//            }
+//
+//        });
 
         Intent intent = getIntent();
-        String message = intent.getStringExtra(ListeningActivity.ARTICLE_MESSAGE);
+
+        message = intent.getStringExtra(ListeningActivity.ARTICLE_MESSAGE);
+
+        //if(message != null) {
+        //    customAdapter.addItem(message);
+       // }
         //only for test, replace by adding checkbox to list
-        Toast.makeText(getApplicationContext(), message,
-                Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getApplicationContext(), message,
+        //        Toast.LENGTH_SHORT).show();
 
         FloatingActionButton fab = findViewById(R.id.runAddThing);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -53,11 +89,11 @@ public class ScrollingActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
