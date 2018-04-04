fd71af117fc8f380d73406ea0c7e7983b928a6fa
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2018-04-04 16:57:43 +0200

    excercises

--- alegorie/src/P2017/Zadanie4.java
+++ alegorie/src/P2017/Zadanie4.java
@@ -17,22 +17,19 @@ class Numbers {
 
 public class Zadanie4 {
 	private int maxCifersSum = 0; 
-	private int indexMaxCifersSum = 0;
+	private int numberMaxCifersSum = 0;
 	private int numberOf35CifersSum = 0;
 	private Numbers[] nums;
+	
+	public static void main(String[] args) {
+	    System.out.println();
+        Zadanie4 zad4 = new Zadanie4();
+        System.out.println(zad4);
+    }
+	
+	
 	public Zadanie4() {
-		File file = new File("dane/R2017/binarne.txt");
-		Numbers[] nums = new Numbers[1000];
-		int i = 0;
-		try {
-		Scanner sc = new Scanner(file);
-		 while(sc.hasNextLine()) {
-			nums[i] = new Numbers(sc.nextInt(), sc.nextInt(), sc.nextInt());
-			i++;
-		 }
-		} catch (FileNotFoundException e) {
-			System.out.println("Brak pliku \n" + e);
-		}
+		load();
 	}
 	
 	private int solv1() {
@@ -57,20 +54,27 @@ public class Zadanie4 {
 	private void solv3() {
 		int all = 0;
 		int n = 0;
-		int index = -1;
 		int max = -1;
+		int[] ciferSumArray = new int[nums.length];
 		for(int i = 0; i < nums.length; i++) {
-			all = ciferSum(nums[i].a) + ciferSum(nums[i].b)
-				+ ciferSum(nums[i].c);
-			if(all > max) {
-				index = i;
-				max = all;
-			}
-			if(all == 35) n++;
+            ciferSumArray[i] = ciferSum(nums[i].a) + ciferSum(nums[i].b)
+                + ciferSum(nums[i].c);
+            all = ciferSum(nums[i].a) + ciferSum(nums[i].b)
+            + ciferSum(nums[i].c);
+            if(all > max) {
+                max = all;
+            }
+            if(all == 35) n++;  
 		}
 		maxCifersSum = max;
-		indexMaxCifersSum = index;
 		numberOf35CifersSum = n;
+		n = 0;
+		for(int i = 0; i < nums.length; i++) {
+			if(ciferSumArray[i] == max) n++;
+		}
+		
+		numberMaxCifersSum = n;
+		
 	}
 	
 	private int NWD(int a, int b) {
@@ -90,21 +94,41 @@ public class Zadanie4 {
 	
 	private int ciferSum(int a) {
 		int sum = 0;
-		while(sum > 0) {
+		while(a > 0) {
 			sum += a % 10;
 			a /= 10;
 		}
 		return sum;
 	}
 	
+	private void load() {
+	    File file = new File("dane/P2017/liczby.txt");
+        nums = new Numbers[1000];
+        int i = 0;
+        Scanner sc = null;
+        try {
+        sc = new Scanner(file);
+         while(sc.hasNextInt()) {
+            if(i < 1000)
+                nums[i] = new Numbers(sc.nextInt(), sc.nextInt(), sc.nextInt());
+            i++;
+         }
+        } catch (FileNotFoundException e) {
+            System.out.println("Brak pliku \n" + e);
+        } finally {
+           if(sc != null) sc.close();
+        }
+	}
+	
 	@Override
 	public String toString() {
 		StringBuffer sb = new StringBuffer();
-	    sb.append(String.format("1) uporządkowanych rosnąco %d", solv1()));
-	    sb.append(String.format("2) suma NWD %d", solv2()));
+	    sb.append(String.format("1) uporządkowanych rosnąco %d \n", solv1()));
+	    sb.append(String.format("2) suma NWD %d \n", solv2()));
+	    solv3();
 	    sb.append(String.format("3) linii z sumą cyfr 35 jest %d \n" +
 	    		"maksymalna suma cyfr to %d " + " i jest w linii nr %d"
-	    		,numberOf35CifersSum, maxCifersSum, indexMaxCifersSum ));
+	    		,numberOf35CifersSum, maxCifersSum, numberMaxCifersSum ));
 		return sb.toString();
 	}
 }
